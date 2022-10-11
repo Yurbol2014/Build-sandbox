f75eb221e841a34ad60ed156f24ed8b988966eab
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2022-10-11 23:51:15 +0300

    Работа с jsx

--- src/main.js
+++ src/main.js
@@ -1,21 +1,39 @@
 
 
-class App {
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => <p>Hello</p>;
+
+ReactDom.render(<App/>,
+ document.getElementById('root'))
+
+
+
+
+
+
+
+
+
+
+//class App {
 
     
-   run= async( name='World')=>{
-      console.log(`Hello ${name}`);
-      console.log([1,2,[2,4]].flat());
-   }
+//   run= async( name='World')=>{
+//      console.log(`Hello ${name}`);
+//      console.log([1,2,[2,4]].flat());
+//   }
       
 
   
-}
+//}
 
 
-const app = new App()
-app.run()
-.then(()=> console.log('done'))
-.catch(()=> console.log('Error'))
+//const app = new App()
+//app.run()
+//.then(()=> console.log('done'))
+//.catch(()=> console.log('Error'))
 
 
