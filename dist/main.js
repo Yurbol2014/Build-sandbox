356d812e01649c3e4e9f36773840aeb015671c24
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2022-10-19 23:48:20 +0300

    Конфигурация Loaderov

--- dist/main.js
+++ dist/main.js
@@ -16,7 +16,7 @@
   \***********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"ed84b509de3e4fdb70e71386a8c6f7fa.png\");\n\n//# sourceURL=webpack://build-sandbox/./src/react.png?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"images/react-ba36242.png\");\n\n//# sourceURL=webpack://build-sandbox/./src/react.png?");
 
 /***/ }),
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
         use: [
           {
             loader: "file-loader",
+            options: {
+               outputPath: 'images',
+               name: '[name]-[sha1:hash:7].[ext]'
+            }
           },
         ],
       },
