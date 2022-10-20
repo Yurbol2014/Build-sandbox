6275478b11cb79d8459d21e814338ce5fdd45468
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2022-10-20 23:37:02 +0300

    React Babel Loader

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import Log from './log'
-import Calc from './calc';
+import React from 'react'
+import  ReactDOM  from 'react-dom'
 
-import img from './react.png'
 
-const calc = new Calc();
-const log = new Log();
 
-log.log(calc.add(1,2,3));
 
-const el = document.createElement('img')
-el.src = img;
-document.body.appendChild(el);
+
+
+const App=() => <p>Hello</p>;
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,18 @@ module.exports = {
 
   module: {
     rules: [
+
+    {
+
+       test: /\.js$/,
+       exclude: /node_modules/,
+       use: [
+         {
+            loader: 'babel-loader'
+         }
+       ]
+    },
+
       //
       // Loading images
       //
